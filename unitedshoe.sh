e8609ad5a79c9914fe17948991c1b98c63ff19ce
Dan Sheikh <dan.sheikh@bcgdv.com>
2018-02-05 13:16:03 -0800

    Added help function. Fixed Sdkman and Spring Boot CLI verification.

--- unitedshoe.sh
+++ unitedshoe.sh
@@ -3,43 +3,44 @@
 # Configure command-line arguments.
 OPTS=`getopt -o a:b:d:g:hl:n: -l artifactId:,build:,dependencies:,groupId:,help,language:name: -n 'unitedshoe' -- "$@"`
 
+unitedshoe_help() {
+    echo -e "Usage:\n\t$0 --groupId org.example --artifactId test --dependencies web,jpa --build gradle --language kotlin path-to-project\n\nNote: 'build' and 'language' are optional."
+    exit 0
+}
+
 if [ $? != 0 ]; then
-  echo 'Unable to parse options.' 2>&1
-  exit 1
+    echo 'Unable to parse options.' 2>&1
+    exit 1
 fi
 
 if [ $# -eq 0 ]; then
-  echo -e "Usage:\n\t$0 --groupId org.example --artifactId test --dependencies web,jpa --build gradle --language kotlin path-to-project\n\nNote: 'build' and 'language' are optional."
-  exit 0;
+    unitedshoe_help
 fi
 
 eval set -- "$OPTS"
 
 # Verify installation of Sdkman.
-if [ -z command -v sdk &> /dev/null ]; then
+if [ -s $HOME/.sdkman/bin/sdkman-init.sh ]; then
+    source $HOME/.sdkman/bin/sdkman-init.sh 
+    # Verify installation of Spring Boot CLI.
+    springboot=$(command -v spring 2> /dev/null)
 
-cat <<EOF
+    if [ -z $springboot ]; then
+	cat <<EOF
+Please install Sprint Boot CLI.
+See installation instructions:
+https://docs.spring.io/spring-boot/docs/current/reference/html/getting-started-installing-spring-boot.html#getting-started-sdkman-cli-installation
+EOF
+	exit 1
+    fi
+else
+    cat <<EOF
 Please install Sdkman and Sprint Boot CLI.
 See respective installation instructions:
 http://sdkman.io/install.html
 https://docs.spring.io/spring-boot/docs/current/reference/html/getting-started-installing-spring-boot.html#getting-started-sdkman-cli-installation
 EOF
-
-exit 1
-
-fi
-
-# Verify installation of Spring Boot CLI.
-if [ -z command -v spring &> /dev/null ]; then
-
-cat <<EOF
-Please install Sprint Boot CLI.
-See installation instructions:
-https://docs.spring.io/spring-boot/docs/current/reference/html/getting-started-installing-spring-boot.html#getting-started-sdkman-cli-installation
-EOF
-
-exit 1
-
+    exit 1
 fi
 
 # Establish defaults.
@@ -47,50 +48,60 @@ build="gradle"
 language="java"
 
 while true; do
-  case "$1" in
-    -a | --artifactId) artifactId="$2"; shift 2;;
-    -b | --build) build="$2"; shift 2;;
-    -d | --dependencies) dependencies="$2"; shift 2;;
-    -g | --groupId) groupId="$2"; shift 2;;
-    -h | --help) echo -e "Usage:\n\t$0 --groupId org.example --artifactId test --dependencies web,jpa --build gradle --language kotlin path-to-project\n\nNote: 'build' and 'language' are optional."; exit 0;;
-    -l | --language) language="$2"; shift 2;;
-    -n | --name) name="$2"; shift 2;;
-    --) shift; break;;
-    *) break;;
-  esac
+    case "$1" in
+	-a | --artifactId) artifactId="$2"; shift 2;;
+	-b | --build) build="$2"; shift 2;;
+	-d | --dependencies) dependencies="$2"; shift 2;;
+	-g | --groupId) groupId="$2"; shift 2;;
+	-h | --help) unitedshoe_help;;
+	-l | --language) language="$2"; shift 2;;
+	-n | --name) name="$2"; shift 2;;
+	--) shift; break;;
+	*) break;;
+    esac
 done
 
 # Check for usage errors.
 err_msgs=()
 
 if [ -z $groupId ]; then
-  err_msgs+="Group Id is required. "
+    err_msgs+="Group Id is required. "
 fi
 
 if [ -z $artifactId ]; then
-  err_msgs+="Artifact Id is required. "
+    err_msgs+="Artifact Id is required. "
 fi
 
 if [ -z $dependencies ]; then
-  err_msgs+="Dependencies are required."
+    err_msgs+="Dependencies are required."
 fi
 
 if [ -z $name ]; then
-  err_msgs+="Application name is required."
+    err_msgs+="Application name is required."
 fi
 
 if [ $# -gt 1 ]; then
-  err_msgs+="Excessive arguments provided. See help."
+    err_msgs+="Excessive arguments provided; see help."
 fi
 
 if [ $# -lt 1 ]; then
-  err_msgs+="Path to project is required. See help."
+    err_msgs+="Path to project is required; see help."
 fi
 
 if [ ${#err_msgs[@]} -gt 0 ]; then
-  echo ${err_msgs[@]}
-  exit 1;
+    echo -e "${err_msgs[@]}"
+    exit 1;
 fi
 
 # Execute Spring Boot CLI.
 eval "spring init --groupId $groupId --artifactId $artifactId --build $build --dependencies $dependencies --language $language --name $name $@"
+
+# Setup directories.
+base_path=$(echo "$groupId/$artifactId" | sed 's/\./\//g')
+dirs=('/api/bindings', '/api/contracts', '/api/controllers', '/configurations', '/entities', '/repositories', '/services')
+
+((dir_cnt=${#dirs[@]}-1))
+for idx in $(seq 0 $dir_cnt); do
+    echo "Making directory $base_path/${dirs[idx]}"
+    mkdir -p "$base_path${dirs[idx]}"
+done
