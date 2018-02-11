31223918cfa0f98a11bc9b0a1501756c8b6389a9
Dan Sheikh <dan.sheikh@outlook.com>
2018-02-10 17:31:49 -0800

    Fixed project directory setup.

--- unitedshoe.sh
+++ unitedshoe.sh
@@ -4,8 +4,8 @@
 OPTS=`getopt -o a:b:d:g:hl:n: -l artifactId:,build:,dependencies:,groupId:,help,language:name: -n 'unitedshoe' -- "$@"`
 
 unitedshoe_help() {
-    echo -e "Usage:\n\t$0 --groupId org.example --artifactId test --dependencies web,jpa --build gradle --language kotlin path-to-project\n\nNote: 'build' and 'language' are optional."
-    exit 0
+  echo -e "Usage:\n\t$0 --groupId org.example --artifactId test --dependencies web,jpa --build gradle --language kotlin path-to-project\n\nNote: 'build' and 'language' are optional."
+  exit 0
 }
 
 if [ $? != 0 ]; then
@@ -26,21 +26,21 @@ if [ -s $HOME/.sdkman/bin/sdkman-init.sh ]; then
     springboot=$(command -v spring 2> /dev/null)
 
     if [ -z $springboot ]; then
-	cat <<EOF
+	      cat <<EOF
 Please install Sprint Boot CLI.
 See installation instructions:
 https://docs.spring.io/spring-boot/docs/current/reference/html/getting-started-installing-spring-boot.html#getting-started-sdkman-cli-installation
 EOF
-	exit 1
+	      exit 1
     fi
 else
-    cat <<EOF
+  cat <<EOF
 Please install Sdkman and Sprint Boot CLI.
 See respective installation instructions:
 http://sdkman.io/install.html
 https://docs.spring.io/spring-boot/docs/current/reference/html/getting-started-installing-spring-boot.html#getting-started-sdkman-cli-installation
 EOF
-    exit 1
+  exit 1
 fi
 
 # Establish defaults.
@@ -48,17 +48,17 @@ build="gradle"
 language="java"
 
 while true; do
-    case "$1" in
-	-a | --artifactId) artifactId="$2"; shift 2;;
-	-b | --build) build="$2"; shift 2;;
-	-d | --dependencies) dependencies="$2"; shift 2;;
-	-g | --groupId) groupId="$2"; shift 2;;
-	-h | --help) unitedshoe_help;;
-	-l | --language) language="$2"; shift 2;;
-	-n | --name) name="$2"; shift 2;;
-	--) shift; break;;
-	*) break;;
-    esac
+  case "$1" in
+	  -a | --artifactId) artifactId="$2"; shift 2;;
+	  -b | --build) build="$2"; shift 2;;
+	  -d | --dependencies) dependencies="$2"; shift 2;;
+	  -g | --groupId) groupId="$2"; shift 2;;
+	  -h | --help) unitedshoe_help;;
+	  -l | --language) language="$2"; shift 2;;
+	  -n | --name) name="$2"; shift 2;;
+	  --) shift; break;;
+	  *) break;;
+  esac
 done
 
 # Check for usage errors.
@@ -90,7 +90,7 @@ fi
 
 if [ ${#err_msgs[@]} -gt 0 ]; then
     echo -e "${err_msgs[@]}"
-    exit 1;
+    exit 1
 fi
 
 # Execute Spring Boot CLI.
@@ -98,10 +98,11 @@ eval "spring init --groupId $groupId --artifactId $artifactId --build $build --d
 
 # Setup directories.
 base_path=$(echo "$groupId/$artifactId" | sed 's/\./\//g')
-dirs=('/api/bindings', '/api/contracts', '/api/controllers', '/configurations', '/entities', '/repositories', '/services')
+src_path="src/main/java"
+dirs=('api/bindings' 'api/contracts' 'api/controllers' 'configurations' 'entities' 'repositories' 'services')
 
 ((dir_cnt=${#dirs[@]}-1))
 for idx in $(seq 0 $dir_cnt); do
-    echo "Making directory $base_path/${dirs[idx]}"
-    mkdir -p "$base_path${dirs[idx]}"
+  echo "Making directory $1/$src_path/$base_path/${dirs[idx]}"
+  mkdir -p "$1/$src_path/$base_path/${dirs[idx]}"
 done
